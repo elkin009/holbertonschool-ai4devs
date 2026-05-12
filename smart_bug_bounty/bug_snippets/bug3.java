97689a28960b956d0055cdb0b2bcc8678c1e1871
elkin009 <elkinhaciyev0@gmail.com>
2026-05-12 14:44:01 +0400

    Update bug3.java

--- smart_bug_bounty/bug_snippets/bug3.java
+++ smart_bug_bounty/bug_snippets/bug3.java
@@ -1,6 +1,13 @@
-public class Bug3 {
+
+public class UserCheck {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        System.out.println(arr[3]);
+        String user1 = new String("Alice");
+        String user2 = new String("Alice");
+        // BUG: Using == for object comparison instead of .equals()
+        if (user1 == user2) {
+            System.out.println("Users are the same.");
+        } else {
+            System.out.println("Users are different.");
+        }
     }
 }
