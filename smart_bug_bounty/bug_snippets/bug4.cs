141ff699d595dfe025eaf886d3c830ac0ec62936
elkin009 <elkinhaciyev0@gmail.com>
2026-05-12 15:08:57 +0400

    Update bug4.cs

--- smart_bug_bounty/bug_snippets/bug4.cs
+++ smart_bug_bounty/bug_snippets/bug4.cs
@@ -1,17 +1,13 @@
-
 using System;
 using System.IO;
 
-class FileLoader {
-    public void ReadMyFile(string path) {
-        Console.WriteLine("Dosya okunuyor: " + path);
-
-        // BUG: StreamReader objesi kapatılmıyor (Close/Dispose yok)
-        // Bu durum bellek sızıntısına ve dosyanın kilitli kalmasına yol açar.
-        StreamReader reader = new StreamReader(path);
-        string data = reader.ReadToEnd();
-        
-        Console.WriteLine("Veri Okundu. Uzunluk: " + data.Length);
-        // reader.Close(); eksik
+class Loader {
+    public void Read(string path) {
+        // BUG
+        StreamReader r = new StreamReader(path);
+        string d = r.ReadToEnd();
+        Console.WriteLine(d);
     }
 }
+// Line 11
+// Line 12
