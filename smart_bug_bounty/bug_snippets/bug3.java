db81c3ada8f7e853a098b972f21af0334d078b81
elkin009 <elkinhaciyev0@gmail.com>
2026-05-12 15:08:37 +0400

    Update bug3.java

--- smart_bug_bounty/bug_snippets/bug3.java
+++ smart_bug_bounty/bug_snippets/bug3.java
@@ -1,17 +1,13 @@
-public class PasswordChecker {
+public class Main {
     public static void main(String[] args) {
-        // İki farklı String objesi oluşturuluyor
-        String storedPass = new String("secure123");
-        String enteredPass = new String("secure123");
-
-        System.out.println("Şifre kontrol ediliyor...");
-
-        // BUG: İçerik karşılaştırmak için .equals() yerine == kullanılıyor
-        // Bu, değerleri değil bellekteki adresleri karşılaştırır.
-        if (enteredPass == storedPass) {
-            System.out.println("Giriş Başarılı!");
+        String s1 = new String("test");
+        String s2 = new String("test");
+        // BUG
+        if (s1 == s2) {
+            System.out.println("Equal");
         } else {
-            System.out.println("Hatalı Şifre: Referanslar uyuşmuyor.");
+            System.out.println("Not Equal");
         }
     }
 }
+// Line 13
