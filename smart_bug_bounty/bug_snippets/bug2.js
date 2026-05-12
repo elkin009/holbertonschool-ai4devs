2b29e821173b75fd095283323bc2e4d58bfd7f59
elkin009 <elkinhaciyev0@gmail.com>
2026-05-12 14:43:14 +0400

    Update bug2.js

--- smart_bug_bounty/bug_snippets/bug2.js
+++ smart_bug_bounty/bug_snippets/bug2.js
@@ -1,3 +1,13 @@
-function calculateTotal(price, tax) {
-    return price + tax;
+function calculateTotal(prices) {
+    let total = 0;
+    // BUG: Using 'in' instead of 'of' with an array
+    for (let price in prices) {
+        total += prices[price];
+    }
+    return total;
 }
+
+const items = [10.5, 20, 5];
+console.log("Total is: " + calculateTotal(items));
+
+
