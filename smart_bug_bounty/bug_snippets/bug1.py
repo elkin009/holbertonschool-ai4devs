1c82591d3a44d025a53b9034c2813d137a96139d
elkin009 <elkinhaciyev0@gmail.com>
2026-05-12 15:10:40 +0400

    Update bug1.py

--- smart_bug_bounty/bug_snippets/bug1.py
+++ smart_bug_bounty/bug_snippets/bug1.py
@@ -3,10 +3,12 @@ def get_last_elements(items, n):
         return "Error"
     if n <= 0:
         return []
-    # BUG
-    return items[len(items) - n + 1:]
+    # BUG: Off-by-one
+    result = items[len(items) - n + 1:]
+    return result
 
 data = [1, 2, 3, 4, 5]
 print(get_last_elements(data, 3))
-# Line 10
 # Line 11
+# Line 12
+# Line 13
